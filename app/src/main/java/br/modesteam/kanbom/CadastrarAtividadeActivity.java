b039cef9345e37511b7b4585d6229ff97f2478c2
Luis <ricardo_canela123@hotmail.com>
2015-04-10 17:26:56 +0000

    luis comitando - layout cadastra atividade

--- app/src/main/java/br/modesteam/kanbom/CadastrarAtividadeActivity.java
+++ app/src/main/java/br/modesteam/kanbom/CadastrarAtividadeActivity.java
@@ -1,16 +1,20 @@
 package br.modesteam.kanbom;
 
-
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 /**
  * Created by ricardo on 10/04/15.
  */
 public class CadastrarAtividadeActivity extends ActionBarActivity {
+    ArrayList<Atividade> atividades = new ArrayList<Atividade>();
+    Atividade atividade = new Atividade();
+
+    atividade.setNome("A");
 
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastraratividade);
